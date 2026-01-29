c0994d2f7729535e288b8635612043fecfc22e14
TwelveIQ <twelveiq2@gmail.com>
2026-01-30 02:42:37 +0630

    Add files via upload

--- Network-mode-switcher.sh
+++ Network-mode-switcher.sh
@@ -6,19 +6,27 @@ systemctl stop wpa_supplicant
 read -p "Network Interface: " network_interface
 read -p "MAC Address: " mac_address
 read -p "Mode(managed/monitor): " mode
+network_interface_mon="${network_interface}mon"
 
-ip link set dev $network_interface down
+ip link set dev $network_interface down 2>/dev/null
+ip link set dev $network_interface_mon down 2>/dev/null
 
-if [[ "$mac_address" == "" ]];
-then
+if [[ "$mac_address" == "" ]]; then
 	echo "Default MAC Address."
 else
 	ip link set dev $network_interface address $mac_address
-fi	
+fi
 
-iwconfig $network_interface mode $mode
+if [[ "$mode" == "monitor" ]]; then
 
-systemctl restart NetworkManager
-systemctl restart wpa_supplicant
+       	iw dev $network_interface interface add $network_interface_mon type monitor
+        ip link set $network_interface_mon up
 
-ip link set dev $network_interface up
+elif [[ "$mode" == "managed" ]]; then
+	
+	ip link set dev $network_interface_mon down 2>/dev/null
+	iw dev $network_interface_mon del 2>/dev/null
+	ip link set $network_interface up
+	systemctl restart NetworkManager wpa_supplicant
+
+fi
