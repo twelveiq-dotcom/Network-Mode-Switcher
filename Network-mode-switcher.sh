1f3f168cfeacd1d93b40a016a23cc28534f5660b
TwelveIQ <twelveiq2@gmail.com>
2026-02-02 01:00:35 +0630

    Update Network-mode-switcher.sh

--- Network-mode-switcher.sh
+++ Network-mode-switcher.sh
@@ -25,6 +25,7 @@ if [[ "$mode" == "monitor" ]]; then
 elif [[ "$mode" == "managed" ]]; then
 	
 	ip link set dev $network_interface_mon down 2>/dev/null
+	iw dev $network_interface set type $mode
 	iw dev $network_interface_mon del 2>/dev/null
 	ip link set $network_interface up
 	systemctl restart NetworkManager wpa_supplicant
